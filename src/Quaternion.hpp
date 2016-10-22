5dffc8a4a9e69dff9478a2a5ab3206d407ec3fd1
Eric Phillips <eric.d.phillips@icloud.com>
2016-10-21 22:45:31 -0400

    Add initial quaternion class

---
+++ src/Quaternion.hpp
@@ -0,0 +1,249 @@
+/**
+ *  ============================================================================
+ *  MIT License
+ *
+ *  Copyright (c) 2016 Eric Phillips
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *  ============================================================================
+ *
+ *
+ *  This file implements a series of math functions for manipulating a
+ *  quaternion.
+ *
+ *  Created by Eric Phillips on October 21, 2016.
+ */
+
+#pragma once
+
+#include <math.h>
+
+
+struct Quaternion
+{
+    union
+    {
+        struct
+        {
+            double X;
+            double Y;
+            double Z;
+            double W;
+        };
+        double data[4];
+    };
+
+
+    /**
+     * Static constants.
+     */
+    static const Quaternion Identity;
+
+
+    /**
+     * Constructors.
+     */
+    Quaternion();
+    Quaternion(double data[]);
+    Quaternion(double x, double y, double z, double w);
+
+
+    /**
+     * Returns the conjugate of a quaternion.
+     * @param rotation: The quaternion in question.
+     * @return: A new quaternion.
+     */
+    static Quaternion Conjugate(Quaternion rotation);
+
+    /**
+     * Returns the norm of a quaternion.
+     * @param rotation: The quaternion in question.
+     * @return: A scalar value.
+     */
+    static double Norm(Quaternion rotation);
+
+    /**
+     * Returns a quaternion with identical rotation and a norm of one.
+     * @param rotation: The quaternion in question.
+     * @return: A new quaternion.
+     */
+    static Quaternion Normalized(Quaternion rotation);
+
+
+    /**
+     * Operator overloading.
+     */
+    struct Quaternion& operator+=(const double& rhs);
+    struct Quaternion& operator-=(const double& rhs);
+    struct Quaternion& operator*=(const double &rhs);
+    struct Quaternion& operator/=(const double &rhs);
+    struct Quaternion& operator+=(const Quaternion& rhs);
+    struct Quaternion& operator-=(const Quaternion& rhs);
+    struct Quaternion& operator*=(const Quaternion &rhs);
+};
+
+Quaternion operator-(Quaternion rhs);
+Quaternion operator+(Quaternion lhs, const double rhs);
+Quaternion operator-(Quaternion lhs, const double rhs);
+Quaternion operator*(Quaternion lhs, const double rhs);
+Quaternion operator/(Quaternion lhs, const double rhs);
+Quaternion operator+(const double lhs, Quaternion rhs);
+Quaternion operator-(const double lhs, Quaternion rhs);
+Quaternion operator*(const double lhs, Quaternion rhs);
+Quaternion operator/(const double lhs, Quaternion rhs);
+Quaternion operator+(Quaternion lhs, const Quaternion &rhs);
+Quaternion operator-(Quaternion lhs, const Quaternion &rhs);
+Quaternion operator*(Quaternion lhs, const Quaternion &rhs);
+bool operator==(const Quaternion &lhs, const Quaternion &rhs);
+bool operator!=(const Quaternion &lhs, const Quaternion &rhs);
+
+
+
+/*******************************************************************************
+ * Implementation
+ */
+
+const Quaternion Quaternion::Identity = Quaternion(0, 0, 0, 1);
+
+
+Quaternion::Quaternion() : X(0), Y(0), Z(0), W(1) {}
+Quaternion::Quaternion(double data[]) : X(data[0]), Y(data[1]), Z(data[2]),
+    W(data[3]) {}
+Quaternion::Quaternion(double x, double y, double z, double w) : X(x), Y(y),
+    Z(z), W(w) {}
+
+
+Quaternion Quaternion::Conjugate(Quaternion rotation)
+{
+    return Quaternion(-rotation.X, -rotation.Y, -rotation.Z, rotation.W);
+}
+
+double Quaternion::Norm(Quaternion rotation)
+{
+    return sqrt(rotation.X * rotation.X +
+        rotation.Y * rotation.Y +
+        rotation.Z  * rotation.Z +
+        rotation.W * rotation.W);
+}
+
+Quaternion Quaternion::Normalized(Quaternion rotation)
+{
+    return rotation / Norm(rotation);
+}
+
+
+struct Quaternion& Quaternion::operator+=(const double &rhs)
+{
+    X += rhs;
+    Y += rhs;
+    Z += rhs;
+    W += rhs;
+    return *this;
+}
+
+struct Quaternion& Quaternion::operator-=(const double &rhs)
+{
+    X -= rhs;
+    Y -= rhs;
+    Z -= rhs;
+    W -= rhs;
+    return *this;
+}
+
+struct Quaternion& Quaternion::operator*=(const double &rhs)
+{
+    X *= rhs;
+    Y *= rhs;
+    Z *= rhs;
+    W *= rhs;
+    return *this;
+}
+
+struct Quaternion& Quaternion::operator/=(const double &rhs)
+{
+    X /= rhs;
+    Y /= rhs;
+    Z /= rhs;
+    W /= rhs;
+    return *this;
+}
+
+struct Quaternion& Quaternion::operator+=(const Quaternion &rhs)
+{
+    X += rhs.X;
+    Y += rhs.Y;
+    Z += rhs.Z;
+    W += rhs.W;
+    return *this;
+}
+
+struct Quaternion& Quaternion::operator-=(const Quaternion &rhs)
+{
+    X -= rhs.X;
+    Y -= rhs.Y;
+    Z -= rhs.Z;
+    W -= rhs.W;
+    return *this;
+}
+
+struct Quaternion& Quaternion::operator*=(const Quaternion &rhs)
+{
+    Quaternion q;
+    q.W = W * rhs.W - X * rhs.X - Y * rhs.Y - Z * rhs.Z;
+    q.X = X * rhs.W + W * rhs.X + Y * rhs.Z - Z * rhs.Y;
+    q.Y = W * rhs.Y - X * rhs.Z + Y * rhs.W + Z * rhs.X;
+    q.Z = W * rhs.Z + X * rhs.Y - Y * rhs.X + Z * rhs.W;
+    *this = q;
+    return *this;
+}
+
+Quaternion operator-(Quaternion rhs) { return rhs * -1; }
+Quaternion operator+(Quaternion lhs, const double rhs) { return lhs += rhs; }
+Quaternion operator-(Quaternion lhs, const double rhs) { return lhs -= rhs; }
+Quaternion operator*(Quaternion lhs, const double rhs) { return lhs *= rhs; }
+Quaternion operator/(Quaternion lhs, const double rhs) { return lhs /= rhs; }
+Quaternion operator+(const double lhs, Quaternion rhs) { return rhs += lhs; }
+Quaternion operator-(const double lhs, Quaternion rhs) { return rhs -= lhs; }
+Quaternion operator*(const double lhs, Quaternion rhs) { return rhs *= lhs; }
+Quaternion operator/(const double lhs, Quaternion rhs) { return rhs /= lhs; }
+Quaternion operator+(Quaternion lhs, const Quaternion &rhs)
+{
+    return lhs += rhs;
+}
+Quaternion operator-(Quaternion lhs, const Quaternion &rhs)
+{
+    return lhs -= rhs;
+}
+Quaternion operator*(Quaternion lhs, const Quaternion &rhs)
+{
+    return lhs *= rhs;
+}
+
+bool operator==(const Quaternion &lhs, const Quaternion &rhs)
+{
+    return lhs.X == rhs.X &&
+        lhs.Y == rhs.Y &&
+        lhs.Z == rhs.Z &&
+        lhs.W == rhs.W;
+}
+
+bool operator!=(const Quaternion &lhs, const Quaternion &rhs)
+{
+    return !(lhs == rhs);
+}
---
+++ test/QuaternionTest.cpp
@@ -0,0 +1,316 @@
+/**
+ *  ============================================================================
+ *  MIT License
+ *
+ *  Copyright (c) 2016 Eric Phillips
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *  ============================================================================
+ *
+ *
+ *  This file contains test cases for the Quaternion functions.
+ *
+ *  Created by Eric Phillips on October 21, 2016.
+ */
+
+#include "catch.hpp"
+#include "Quaternion.hpp"
+
+
+TEST_CASE("Quaternion plus scalar", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q = Quaternion(2, -5, 3, 7);
+    q = q + 3;
+    CHECK(q.X == Approx(5));
+    CHECK(q.Y == Approx(-2));
+    CHECK(q.Z == Approx(6));
+    CHECK(q.W == Approx(10));
+    // Case 2
+    q = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q = q + 0.2;
+    CHECK(q.X == Approx(0.44));
+    CHECK(q.Y == Approx(0.2082));
+    CHECK(q.Z == Approx(-0.1));
+    CHECK(q.W == Approx(0.8));
+    // Case 3
+    q = Quaternion(-27, 83, 32, -153);
+    q = q + 13;
+    CHECK(q.X == Approx(-14));
+    CHECK(q.Y == Approx(96));
+    CHECK(q.Z == Approx(45));
+    CHECK(q.W == Approx(-140));
+}
+
+TEST_CASE("Quaternion minus scalar", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q = Quaternion(2, -5, 3, 7);
+    q = q - 3;
+    CHECK(q.X == Approx(-1));
+    CHECK(q.Y == Approx(-8));
+    CHECK(q.Z == Approx(0));
+    CHECK(q.W == Approx(4));
+    // Case 2
+    q = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q = q - 0.2;
+    CHECK(q.X == Approx(0.04));
+    CHECK(q.Y == Approx(-0.1918));
+    CHECK(q.Z == Approx(-0.5));
+    CHECK(q.W == Approx(0.4));
+    // Case 3
+    q = Quaternion(-27, 83, 32, -153);
+    q = q - 13;
+    CHECK(q.X == Approx(-40));
+    CHECK(q.Y == Approx(70));
+    CHECK(q.Z == Approx(19));
+    CHECK(q.W == Approx(-166));
+}
+
+TEST_CASE("Quaternion times scalar", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q = Quaternion(2, -5, 3, 7);
+    q = q * 3;
+    CHECK(q.X == Approx(6));
+    CHECK(q.Y == Approx(-15));
+    CHECK(q.Z == Approx(9));
+    CHECK(q.W == Approx(21));
+    // Case 2
+    q = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q = q * 0.2;
+    CHECK(q.X == Approx(0.048));
+    CHECK(q.Y == Approx(0.00164));
+    CHECK(q.Z == Approx(-0.06));
+    CHECK(q.W == Approx(0.12));
+    // Case 3
+    q = Quaternion(-27, 83, 32, -153);
+    q = q * 13;
+    CHECK(q.X == Approx(-351));
+    CHECK(q.Y == Approx(1079));
+    CHECK(q.Z == Approx(416));
+    CHECK(q.W == Approx(-1989));
+}
+
+TEST_CASE("Quaternion divided by scalar", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q = Quaternion(2, -5, 3, 7);
+    q = q / 3;
+    CHECK(q.X == Approx(0.66666667));
+    CHECK(q.Y == Approx(-1.66666667));
+    CHECK(q.Z == Approx(1));
+    CHECK(q.W == Approx(2.33333333));
+    // Case 2
+    q = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q = q / 0.2;
+    CHECK(q.X == Approx(1.2));
+    CHECK(q.Y == Approx(0.041));
+    CHECK(q.Z == Approx(-1.5));
+    CHECK(q.W == Approx(3));
+    // Case 3
+    q = Quaternion(-27, 83, 32, -153);
+    q = q / 13;
+    CHECK(q.X == Approx(-2.0769231));
+    CHECK(q.Y == Approx(6.3846154));
+    CHECK(q.Z == Approx(2.461538462));
+    CHECK(q.W == Approx(-11.76923077));
+}
+
+TEST_CASE("Quaternion plus Quaternion", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q1 = Quaternion(2, -5, 3, 7);
+    Quaternion q2 = Quaternion(6, 2, -1, 9);
+    Quaternion q = q1 + q2;
+    CHECK(q.X == Approx(8));
+    CHECK(q.Y == Approx(-3));
+    CHECK(q.Z == Approx(2));
+    CHECK(q.W == Approx(16));
+    // Case 2
+    q1 = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q2 = Quaternion(0.53, -0.0532, 0.22, 0.85);
+    q = q1 + q2;
+    CHECK(q.X == Approx(0.77));
+    CHECK(q.Y == Approx(-0.045));
+    CHECK(q.Z == Approx(-0.08));
+    CHECK(q.W == Approx(1.45));
+    // Case 3
+    q1 = Quaternion(-27, 83, 32, -153);
+    q2 = Quaternion(36, -64, 12, 24);
+    q = q1 + q2;
+    CHECK(q.X == Approx(9));
+    CHECK(q.Y == Approx(19));
+    CHECK(q.Z == Approx(44));
+    CHECK(q.W == Approx(-129));
+}
+
+TEST_CASE("Quaternion minus Quaternion", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q1 = Quaternion(2, -5, 3, 7);
+    Quaternion q2 = Quaternion(6, 2, -1, 9);
+    Quaternion q = q1 - q2;
+    CHECK(q.X == Approx(-4));
+    CHECK(q.Y == Approx(-7));
+    CHECK(q.Z == Approx(4));
+    CHECK(q.W == Approx(-2));
+    // Case 2
+    q1 = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q2 = Quaternion(0.53, -0.0532, 0.22, 0.85);
+    q = q1 - q2;
+    CHECK(q.X == Approx(-0.29));
+    CHECK(q.Y == Approx(0.0614));
+    CHECK(q.Z == Approx(-0.52));
+    CHECK(q.W == Approx(-0.25));
+    // Case 3
+    q1 = Quaternion(-27, 83, 32, -153);
+    q2 = Quaternion(36, -64, 12, 24);
+    q = q1 - q2;
+    CHECK(q.X == Approx(-63));
+    CHECK(q.Y == Approx(147));
+    CHECK(q.Z == Approx(20));
+    CHECK(q.W == Approx(-177));
+}
+
+TEST_CASE("Quaternion times Quaternion", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q1 = Quaternion(0.3919183, 0.3196269, -0.8430416, -0.1830837);
+    Quaternion q2 = Quaternion(0.3535534, -0.1464466, 0.3535534, 0.8535535);
+    Quaternion q = q1 * q2;
+    CHECK(q.X == Approx(0.25933797330736996));
+    CHECK(q.Y == Approx(-0.13699362693909));
+    CHECK(q.Z == Approx(-0.9547112526844199));
+    CHECK(q.W == Approx(0.050032716379809994));
+    // Case 2
+    q1 = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q2 = Quaternion(0.53, -0.0532, 0.22, 0.85);
+    q = q1 * q2;
+    CHECK(q.X == Approx(0.507844));
+    CHECK(q.Y == Approx(-0.23675));
+    CHECK(q.Z == Approx(-0.140114));
+    CHECK(q.W == Approx(0.44923624));
+    // Case 3
+    q1 = Quaternion(0, 0.7071068, 0, 0.7071068);
+    q2 = Quaternion(0, 0.7071068, 0, 0.7071068);
+    q = q1 * q2;
+    CHECK(q.X == Approx(0));
+    CHECK(q.Y == Approx(1));
+    CHECK(q.Z == Approx(0));
+    CHECK(q.W == Approx(0));
+}
+
+TEST_CASE("Quaternion equality", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q1 = Quaternion(2, -5, 3, 7);
+    Quaternion q2 = Quaternion(6, 2, -1, 9);
+    CHECK_FALSE(q1 == q2);
+    // Case 2
+    q1 = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q2 = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    CHECK(q1 == q2);
+    // Case 3
+    q1 = Quaternion(-27, 83, 32, -153);
+    q2 = Quaternion(-27, 84, 32, -153);
+    CHECK_FALSE(q1 == q2);
+}
+
+TEST_CASE("Quaternion inequality", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q1 = Quaternion(2, -5, 3, 7);
+    Quaternion q2 = Quaternion(6, 2, -1, 9);
+    CHECK(q1 != q2);
+    // Case 2
+    q1 = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    q2 = Quaternion(0.24, 0.0082, -0.3, 0.6);
+    CHECK_FALSE(q1 != q2);
+    // Case 3
+    q1 = Quaternion(-27, 83, 32, -153);
+    q2 = Quaternion(-27, 84, 32, -153);
+    CHECK(q1 != q2);
+}
+
+TEST_CASE("Quaternion conjugate", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q1 = Quaternion(0.3919183, 0.3196269, -0.8430416, -0.1830837);
+    Quaternion q = Quaternion::Conjugate(q1);
+    CHECK(q.X == Approx(-0.3919183));
+    CHECK(q.Y == Approx(-0.3196269));
+    CHECK(q.Z == Approx(0.8430416));
+    CHECK(q.W == Approx(-0.1830837));
+    // Case 2
+    q1 = Quaternion(0, 0.7071068, 0, 0.7071068);
+    q = Quaternion::Conjugate(q1);
+    CHECK(q.X == Approx(0));
+    CHECK(q.Y == Approx(-0.7071068));
+    CHECK(q.Z == Approx(0));
+    CHECK(q.W == Approx(0.7071068));
+    // Case 3
+    q1 = Quaternion(0.3535534, -0.1464466, 0.3535534, 0.8535535);
+    q = Quaternion::Conjugate(q1);
+    CHECK(q.X == Approx(-0.3535534));
+    CHECK(q.Y == Approx(0.1464466));
+    CHECK(q.Z == Approx(-0.3535534));
+    CHECK(q.W == Approx(0.8535535));
+}
+
+TEST_CASE("Quaternion norm", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q1 = Quaternion(0.3919183, 0.3196269, -0.8430416, -0.1830837);
+    double n = Quaternion::Norm(q1);
+    CHECK(n == Approx(1));
+    // Case 2
+    q1 = Quaternion(0, 0.7071068, 0, 0.7071068);
+    n = Quaternion::Norm(q1);
+    CHECK(n == Approx(1));
+    // Case 3
+    q1 = Quaternion(0.3535534, -0.1464466, 0.3535534, 0.8535535);
+    n = Quaternion::Norm(q1);
+    CHECK(n == Approx(1));
+}
+
+TEST_CASE("Quaternion normalized", "[Quaternion]")
+{
+    // Case 1
+    Quaternion q1 = Quaternion(0.4, 0.2, -1.3, -0.5);
+    Quaternion q = Quaternion::Normalized(q1);
+    CHECK(q.X == Approx(0.2734343708));
+    CHECK(q.Y == Approx(0.1367171854));
+    CHECK(q.Z == Approx(-0.888661705));
+    CHECK(q.W == Approx(-0.3417929635));
+    // Case 2
+    q1 = Quaternion(0, 0, 0, 0.2);
+    q = Quaternion::Normalized(q1);
+    CHECK(q.X == Approx(0));
+    CHECK(q.Y == Approx(0));
+    CHECK(q.Z == Approx(0));
+    CHECK(q.W == Approx(1));
+    // Case 3
+    q1 = Quaternion(0.5, 0.5, -0.5, 0.5);
+    q = Quaternion::Normalized(q1);
+    CHECK(q.X == Approx(0.5));
+    CHECK(q.Y == Approx(0.5));
+    CHECK(q.Z == Approx(-0.5));
+    CHECK(q.W == Approx(0.5));
+}
