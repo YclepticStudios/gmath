1c16b868c0caffbe5cad749909a905367a0c2183
Eric Phillips <eric.d.phillips@icloud.com>
2016-11-12 07:54:47 -0500

    Initial matrix3x3 Opperator overloading, constants, and Transpose implemented

---
+++ src/Matrix3x3.hpp
@@ -0,0 +1,371 @@
+/**
+ *  ============================================================================
+ *  MIT License
+ *
+ *  Copyright (c) 2016 Eric Phillips
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *  ============================================================================
+ *
+ *
+ *  This file implements a series of math functions for manipulating a
+ *  3x3 matrix.
+ *
+ *  Created by Eric Phillips on November 8, 2016.
+ */
+
+#pragma once
+
+#include <math.h>
+
+
+
+/**
+ * Attempt to include a header file if the file exists.
+ * If the file does not exist, create a dummy data structure for that type.
+ * If it cannot be determined if it exists, just attempt to include it.
+ */
+#ifdef __has_include
+#   if __has_include("Vector3.hpp")
+#       include "Vector3.hpp"
+#   else
+        struct Vector3
+        {
+            union
+            {
+                struct
+                {
+                    double X;
+                    double Y;
+                    double Z;
+                };
+                double data[3];
+            };
+
+            inline Vector3() : X(0), Y(0), Z(0) {}
+            inline Vector3(double data[]) : X(data[0]), Y(data[1]), Z(data[2])
+                {}
+            inline Vector3(double value) : X(value), Y(value), Z(value) {}
+            inline Vector3(double x, double y) : X(x), Y(y), Z(0) {}
+            inline Vector3(double x, double y, double z) : X(x), Y(y), Z(z) {}
+        };
+#   endif
+#else
+#   include "Vector3.hpp"
+#endif
+
+
+/**
+ * Attempt to include a header file if the file exists.
+ * If the file does not exist, create a dummy data structure for that type.
+ * If it cannot be determined if it exists, just attempt to include it.
+ */
+#ifdef __has_include
+#   if __has_include("Quaternion.hpp")
+#       include "Quaternion.hpp"
+#   else
+        struct Quaternion
+        {
+            union
+            {
+                struct
+                {
+                    double X;
+                    double Y;
+                    double Z;
+                    double W;
+                };
+                double data[4];
+            };
+
+            inline Quaternion() : X(0), Y(0), Z(0), W(1) {}
+            inline Quaternion(double data[]) : X(data[0]), Y(data[1]),
+                Z(data[2]), W(data[3]) {}
+            inline Quaternion(Vector3 vector, double scalar) : X(vector.X),
+                Y(vector.Y), Z(vector.Z), W(scalar) {}
+            inline Quaternion(double x, double y, double z, double w) : X(x),
+                Y(y), Z(z), W(w) {}
+        };
+#   endif
+#else
+#   include "Quaternion.hpp"
+#endif
+
+
+struct Matrix3x3
+{
+    union
+    {
+        struct
+        {
+            double D00;
+            double D01;
+            double D02;
+            double D10;
+            double D11;
+            double D12;
+            double D20;
+            double D21;
+            double D22;
+        };
+        double data[3][3];
+    };
+
+
+    /**
+     * Constructors.
+     */
+    inline Matrix3x3();
+    inline Matrix3x3(double data[]);
+    inline Matrix3x3(Vector3 row0, Vector3 row1, Vector3 row2);
+    inline Matrix3x3(double d00, double d01, double d02, double d10, double d11,
+        double d12, double d20, double d21, double d22);
+
+
+    /**
+     * Constants for common Matrix3x3.
+     */
+    static inline Matrix3x3 Identity();
+    static inline Matrix3x3 Zero();
+    static inline Matrix3x3 One();
+
+
+    /**
+     * Converts a quaternion to a rotation matrix.
+     * @param rotation: The input quaternion.
+     * @return: A new rotation matrix.
+     */
+    static inline Matrix3x3 FromQuaternion(Quaternion rotation);
+
+    /**
+     * Returns the inverse of a matrix.
+     * @param matrix: The input matrix.
+     * @return: A new matrix.
+     */
+    static inline Matrix3x3 Inverse(Matrix3x3 matrix);
+
+    /**
+     * Returns true if a matrix is invertable.
+     * @param matrix: The input matrix.
+     * @return: A new matrix.
+     */
+    static inline bool IsInvertable(Matrix3x3 matrix);
+
+    /**
+     * Multiplies two matrices element-wise.
+     * @param a: The left-hand side of the multiplication.
+     * @param b: The right-hand side of the multiplication.
+     * @return: A new matrix.
+     */
+    static inline Matrix3x3 Scale(Matrix3x3 a, Matrix3x3 b);
+
+    /**
+     * Converts a rotation matrix to a quaternion.
+     * @param rotation: The input rotation matrix.
+     * @return: A new quaternion.
+     */
+    static inline Quaternion ToQuaternion(Matrix3x3 rotation);
+
+    /**
+     * Returns the transpose of a matrix.
+     * @param matrix: The input matrix.
+     * @return: A new matrix.
+     */
+    static inline Matrix3x3 Transpose(Matrix3x3 matrix);
+
+    /**
+     * Operator overloading.
+     */
+    inline struct Matrix3x3& operator+=(const double rhs);
+    inline struct Matrix3x3& operator-=(const double rhs);
+    inline struct Matrix3x3& operator*=(const double rhs);
+    inline struct Matrix3x3& operator/=(const double rhs);
+    inline struct Matrix3x3& operator+=(const Matrix3x3 rhs);
+    inline struct Matrix3x3& operator-=(const Matrix3x3 rhs);
+    inline struct Matrix3x3& operator*=(const Matrix3x3 rhs);
+};
+
+inline Matrix3x3 operator-(Matrix3x3 rhs);
+inline Matrix3x3 operator+(Matrix3x3 lhs, const double rhs);
+inline Matrix3x3 operator-(Matrix3x3 lhs, const double rhs);
+inline Matrix3x3 operator*(Matrix3x3 lhs, const double rhs);
+inline Matrix3x3 operator/(Matrix3x3 lhs, const double rhs);
+inline Matrix3x3 operator+(const double lhs, Matrix3x3 rhs);
+inline Matrix3x3 operator-(const double lhs, Matrix3x3 rhs);
+inline Matrix3x3 operator*(const double lhs, Matrix3x3 rhs);
+inline Matrix3x3 operator+(Matrix3x3 lhs, const Matrix3x3 rhs);
+inline Matrix3x3 operator-(Matrix3x3 lhs, const Matrix3x3 rhs);
+inline Matrix3x3 operator*(Matrix3x3 lhs, const Matrix3x3 rhs);
+inline Vector3 operator*(Matrix3x3 lhs, const Vector3 rhs);
+inline bool operator==(const Matrix3x3 lhs, const Matrix3x3 rhs);
+inline bool operator!=(const Matrix3x3 lhs, const Matrix3x3 rhs);
+
+
+
+/*******************************************************************************
+ * Implementation
+ */
+
+Matrix3x3::Matrix3x3() : D00(1), D01(0), D02(0), D10(0), D11(1), D12(0), D20(0),
+    D21(0), D22(1) {}
+Matrix3x3::Matrix3x3(double data[]) : D00(data[0]), D01(data[1]), D02(data[2]),
+    D10(data[3]), D11(data[4]), D12(data[5]), D20(data[6]), D21(data[7]),
+    D22(data[8]) {}
+Matrix3x3::Matrix3x3(Vector3 row0, Vector3 row1, Vector3 row2) : D00(row0.X),
+    D01(row0.Y), D02(row0.Z), D10(row1.X), D11(row1.Y), D12(row1.Z),
+    D20(row2.X), D21(row2.Y), D22(row2.Z) {}
+Matrix3x3::Matrix3x3(double d00, double d01, double d02, double d10, double d11,
+    double d12, double d20, double d21, double d22) : D00(d00), D01(d01),
+    D02(d02), D10(d10), D11(d11), D12(d12), D20(d20), D21(d21), D22(d22) {}
+
+
+Matrix3x3 Matrix3x3::Identity()
+{
+    return Matrix3x3(1, 0, 0, 0, 1, 0, 0, 0, 1);
+}
+
+Matrix3x3 Matrix3x3::Zero()
+{
+    return Matrix3x3(0, 0, 0, 0, 0, 0, 0, 0, 0);
+}
+
+Matrix3x3 Matrix3x3::One()
+{
+    return Matrix3x3(1, 1, 1, 1, 1, 1, 1, 1, 1);
+}
+
+
+Matrix3x3 Matrix3x3::Transpose(Matrix3x3 matrix)
+{
+    double tmp;
+    tmp = matrix.D01;
+    matrix.D01 = matrix.D10;
+    matrix.D10 = tmp;
+    tmp = matrix.D02;
+    matrix.D02 = matrix.D20;
+    matrix.D20 = tmp;
+    tmp = matrix.D12;
+    matrix.D12 = matrix.D21;
+    matrix.D21 = tmp;
+    return matrix;
+}
+
+
+struct Matrix3x3& Matrix3x3::operator+=(const double rhs)
+{
+    D00 += rhs; D01 += rhs; D02 += rhs;
+    D10 += rhs; D11 += rhs; D12 += rhs;
+    D20 += rhs; D21 += rhs; D22 += rhs;
+    return *this;
+}
+
+struct Matrix3x3& Matrix3x3::operator-=(const double rhs)
+{
+    D00 -= rhs; D01 -= rhs; D02 -= rhs;
+    D10 -= rhs; D11 -= rhs; D12 -= rhs;
+    D20 -= rhs; D21 -= rhs; D22 -= rhs;
+    return *this;
+}
+
+struct Matrix3x3& Matrix3x3::operator*=(const double rhs)
+{
+    D00 *= rhs; D01 *= rhs; D02 *= rhs;
+    D10 *= rhs; D11 *= rhs; D12 *= rhs;
+    D20 *= rhs; D21 *= rhs; D22 *= rhs;
+    return *this;
+}
+
+struct Matrix3x3& Matrix3x3::operator/=(const double rhs)
+{
+    D00 /= rhs; D01 /= rhs; D02 /= rhs;
+    D10 /= rhs; D11 /= rhs; D12 /= rhs;
+    D20 /= rhs; D21 /= rhs; D22 /= rhs;
+    return *this;
+}
+
+struct Matrix3x3& Matrix3x3::operator+=(const Matrix3x3 rhs)
+{
+    D00 += rhs.D00; D01 += rhs.D01; D02 += rhs.D02;
+    D10 += rhs.D10; D11 += rhs.D11; D12 += rhs.D12;
+    D20 += rhs.D20; D21 += rhs.D21; D22 += rhs.D22;
+    return *this;
+}
+
+struct Matrix3x3& Matrix3x3::operator-=(const Matrix3x3 rhs)
+{
+    D00 -= rhs.D00; D01 -= rhs.D01; D02 -= rhs.D02;
+    D10 -= rhs.D10; D11 -= rhs.D11; D12 -= rhs.D12;
+    D20 -= rhs.D20; D21 -= rhs.D21; D22 -= rhs.D22;
+    return *this;
+}
+
+struct Matrix3x3& Matrix3x3::operator*=(const Matrix3x3 rhs)
+{
+    Matrix3x3 m;
+    m.D00 = D00 * rhs.D00 + D01 * rhs.D10 + D02 * rhs.D20;
+    m.D01 = D00 * rhs.D01 + D01 * rhs.D11 + D02 * rhs.D21;
+    m.D02 = D00 * rhs.D02 + D01 * rhs.D12 + D02 * rhs.D22;
+    m.D10 = D10 * rhs.D00 + D11 * rhs.D10 + D12 * rhs.D20;
+    m.D11 = D10 * rhs.D01 + D11 * rhs.D11 + D12 * rhs.D21;
+    m.D12 = D10 * rhs.D02 + D11 * rhs.D12 + D12 * rhs.D22;
+    m.D20 = D20 * rhs.D00 + D21 * rhs.D10 + D22 * rhs.D20;
+    m.D21 = D20 * rhs.D01 + D21 * rhs.D11 + D22 * rhs.D21;
+    m.D22 = D20 * rhs.D02 + D21 * rhs.D12 + D22 * rhs.D22;
+    *this = m;
+    return *this;
+}
+
+Matrix3x3 operator-(Matrix3x3 rhs) { return rhs * -1; }
+Matrix3x3 operator+(Matrix3x3 lhs, const double rhs) { return lhs += rhs; }
+Matrix3x3 operator-(Matrix3x3 lhs, const double rhs) { return lhs -= rhs; }
+Matrix3x3 operator*(Matrix3x3 lhs, const double rhs) { return lhs *= rhs; }
+Matrix3x3 operator/(Matrix3x3 lhs, const double rhs) { return lhs /= rhs; }
+Matrix3x3 operator+(const double lhs, Matrix3x3 rhs) { return rhs += lhs; }
+Matrix3x3 operator-(const double lhs, Matrix3x3 rhs) { return rhs -= lhs; }
+Matrix3x3 operator*(const double lhs, Matrix3x3 rhs) { return rhs *= lhs; }
+Matrix3x3 operator+(Matrix3x3 lhs, const Matrix3x3 rhs) { return lhs += rhs; }
+Matrix3x3 operator-(Matrix3x3 lhs, const Matrix3x3 rhs) { return lhs -= rhs; }
+Matrix3x3 operator*(Matrix3x3 lhs, const Matrix3x3 rhs) { return lhs *= rhs; }
+
+Vector3 operator*(Matrix3x3 lhs, const Vector3 rhs)
+{
+    Vector3 v;
+    v.X = lhs.D00 * rhs.X + lhs.D01 * rhs.Y + lhs.D02 * rhs.Z;
+    v.Y = lhs.D10 * rhs.X + lhs.D11 * rhs.Y + lhs.D12 * rhs.Z;
+    v.Z = lhs.D20 * rhs.X + lhs.D21 * rhs.Y + lhs.D22 * rhs.Z;
+    return v;
+}
+
+bool operator==(const Matrix3x3 lhs, const Matrix3x3 rhs)
+{
+    return lhs.D00 == rhs.D00 &&
+        lhs.D01 == rhs.D01 &&
+        lhs.D02 == rhs.D02 &&
+        lhs.D10 == rhs.D10 &&
+        lhs.D11 == rhs.D11 &&
+        lhs.D12 == rhs.D12 &&
+        lhs.D20 == rhs.D20 &&
+        lhs.D21 == rhs.D21 &&
+        lhs.D22 == rhs.D22;
+}
+
+bool operator!=(const Matrix3x3 lhs, const Matrix3x3 rhs)
+{
+    return !(lhs == rhs);
+}
---
+++ test/Matrix3x3Test.cpp
@@ -0,0 +1,269 @@
+/**
+ *  ============================================================================
+ *  MIT License
+ *
+ *  Copyright (c) 2016 Eric Phillips
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
+ *  Software is furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *  ============================================================================
+ *
+ *
+ *  This file contains test cases for the Matrix3x3 functions.
+ *
+ *  Created by Eric Phillips on October 21, 2016.
+ */
+
+#include "catch.hpp"
+#include "Matrix3x3.hpp"
+
+
+#define CHECK_MATRIX(a, b) \
+    CHECK(a.D00 == Approx(b.D00)); \
+    CHECK(a.D01 == Approx(b.D01)); \
+    CHECK(a.D02 == Approx(b.D02)); \
+    CHECK(a.D10 == Approx(b.D10)); \
+    CHECK(a.D11 == Approx(b.D11)); \
+    CHECK(a.D12 == Approx(b.D12)); \
+    CHECK(a.D20 == Approx(b.D20)); \
+    CHECK(a.D21 == Approx(b.D21)); \
+    CHECK(a.D22 == Approx(b.D22));
+
+
+TEST_CASE("Matrix3x3 plus scalar", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    m1 = m1 + 3;
+    Matrix3x3 m2 = Matrix3x3(5, -2, 6, 10, 4, -3, -6, 7, 11);
+    CHECK_MATRIX(m1, m2);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m1 = m1 + 0.2;
+    m2 = Matrix3x3(0.44, 0.2082, -0.1, 0.8, -1.2, 0.93, 0.58, 0.296, 0.04);
+    CHECK_MATRIX(m1, m2);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m1 = m1 + 13;
+    m2 = Matrix3x3(-14, 96, 45, -140, 66, 96, -51, 36, -33);
+    CHECK_MATRIX(m1, m2);
+}
+
+TEST_CASE("Matrix3x3 minus scalar", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    m1 = m1 - 3;
+    Matrix3x3 m2 = Matrix3x3(-1, -8, 0, 4, -2, -9, -12, 1, 5);
+    CHECK_MATRIX(m1, m2);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m1 = m1 - 0.2;
+    m2 = Matrix3x3(0.04, -0.1918, -0.5, 0.4, -1.6, 0.53, 0.18, -0.104, -0.36);
+    CHECK_MATRIX(m1, m2);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m1 = m1 - 13;
+    m2 = Matrix3x3(-40, 70, 19, -166, 40, 70, -77, 10, -59);
+    CHECK_MATRIX(m1, m2);
+}
+
+TEST_CASE("Matrix3x3 times scalar", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    m1 = m1 * 3;
+    Matrix3x3 m2 = Matrix3x3(6, -15, 9, 21, 3, -18, -27, 12, 24);
+    CHECK_MATRIX(m1, m2);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m1 = m1 * 0.2;
+    m2 = Matrix3x3(0.048, 0.00164, -0.06, 0.12, -0.28, 0.146, 0.076, 0.0192,
+        -0.032);
+    CHECK_MATRIX(m1, m2);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m1 = m1 * 13;
+    m2 = Matrix3x3(-351, 1079, 416, -1989, 689, 1079, -832, 299, -598);
+    CHECK_MATRIX(m1, m2);
+}
+
+TEST_CASE("Matrix3x3 divided by scalar", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    m1 = m1 / 3;
+    Matrix3x3 m2 = Matrix3x3(0.6666666667, -1.6666666667, 1, 2.3333333333,
+        0.3333333333, -2, -3, 1.3333333333, 2.6666666667);
+    CHECK_MATRIX(m1, m2);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m1 = m1 / 0.2;
+    m2 = Matrix3x3(1.2, 0.041, -1.5, 3, -7, 3.65, 1.9, 0.48, -0.8);
+    CHECK_MATRIX(m1, m2);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m1 = m1 / 13;
+    m2 = Matrix3x3(-2.0769230769, 6.3846153846, 2.4615384615, -11.7692307692,
+        4.0769230769, 6.3846153846, -4.9230769231, 1.7692307692, -3.5384615385);
+    CHECK_MATRIX(m1, m2);
+}
+
+TEST_CASE("Matrix3x3 plus Matrix3x3", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    Matrix3x3 m2 = Matrix3x3(-14, 96, 45, -140, 66, 96, -51, 36, -33);
+    Matrix3x3 m3 = m1 + m2;
+    Matrix3x3 m4 = Matrix3x3(-12, 91, 48, -133, 67, 90, -60, 40, -25);
+    CHECK_MATRIX(m3, m4);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m2 = Matrix3x3(1.7, 0.003, -5.6, 0.44, 0.2082, -0.1, 0.8, -1.2, 0.93);
+    m3 = m1 + m2;
+    m4 = Matrix3x3(1.94, 0.0112, -5.9, 1.04, -1.1918, 0.63, 1.18, -1.104, 0.77);
+    CHECK_MATRIX(m3, m4);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m2 = Matrix3x3(5, -2, 6, 10, 4, -3, -6, 7, 11);
+    m3 = m1 + m2;
+    m4 = Matrix3x3(-22, 81, 38, -143, 57, 80, -70, 30, -35);
+    CHECK_MATRIX(m3, m4);
+}
+
+TEST_CASE("Matrix3x3 minus Matrix3x3", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    Matrix3x3 m2 = Matrix3x3(-14, 96, 45, -140, 66, 96, -51, 36, -33);
+    Matrix3x3 m3 = m1 - m2;
+    Matrix3x3 m4 = Matrix3x3(16, -101, -42, 147, -65, -102, 42, -32, 41);
+    CHECK_MATRIX(m3, m4);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m2 = Matrix3x3(1.7, 0.003, -5.6, 0.44, 0.2082, -0.1, 0.8, -1.2, 0.93);
+    m3 = m1 - m2;
+    m4 = Matrix3x3(-1.46, 0.0052, 5.3, 0.16, -1.6082, 0.83, -0.42, 1.296,
+        -1.09);
+    CHECK_MATRIX(m3, m4);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m2 = Matrix3x3(5, -2, 6, 10, 4, -3, -6, 7, 11);
+    m3 = m1 - m2;
+    m4 = Matrix3x3(-32, 85, 26, -163, 49, 86, -58, 16, -57);
+    CHECK_MATRIX(m3, m4);
+}
+
+TEST_CASE("Matrix3x3 times Matrix3x3", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    Matrix3x3 m2 = Matrix3x3(-14, 96, 45, -140, 66, 96, -51, 36, -33);
+    Matrix3x3 m3 = m1 * m2;
+    Matrix3x3 m4 = Matrix3x3(519, -30, -489, 68, 522, 609, -842, -312, -285);
+    CHECK_MATRIX(m3, m4);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m2 = Matrix3x3(1.7, 0.003, -5.6, 0.44, 0.2082, -0.1, 0.8, -1.2, 0.93);
+    m3 = m1 * m2;
+    m4 = Matrix3x3(0.1716080, 0.3624272, -1.6238200, 0.9880000, -1.1656800,
+        -2.5411000, 0.5602400, 0.2131272, -2.2864000);
+    CHECK_MATRIX(m3, m4);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m2 = Matrix3x3(5, -2, 6, 10, 4, -3, -6, 7, 11);
+    m3 = m1 * m2;
+    m4 = Matrix3x3(503, 610, -59, -733, 1099, -164, 186, -102, -959);
+    CHECK_MATRIX(m3, m4);
+}
+
+TEST_CASE("Matrix3x3 times Vector3", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    Vector3 v = Vector3(0.3535534, -0.1464466, 0.3535534);
+    Vector3 r = m * v;
+    CHECK(r.X == Approx(2.5));
+    CHECK(r.Y == Approx(0.2071068));
+    CHECK(r.Z == Approx(-0.9393398));
+    // Case 2
+    m = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    v = Vector3(0.53, -0.0532, 0.22);
+    r = m * v;
+    CHECK(r.X == Approx(0.0607638));
+    CHECK(r.Y == Approx(0.5530800));
+    CHECK(r.Z == Approx(0.1610928));
+    // Case 3
+    m = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    v = Vector3(13, -1.23, 3.4);
+    r = m * v;
+    CHECK(r.X == Approx(-344.29));
+    CHECK(r.Y == Approx(-1771.99));
+    CHECK(r.Z == Approx(-1016.69));
+}
+
+TEST_CASE("Matrix3x3 equality", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    Matrix3x3 m2 = Matrix3x3(-14, 96, 45, -140, 66, 96, -51, 36, -33);
+    CHECK_FALSE(m1 == m2);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m2 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    CHECK(m1 == m2);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m2 = Matrix3x3(-27, 83, 33, -153, 53, 83, -64, 23, -46);
+    CHECK_FALSE(m1 == m2);
+}
+
+TEST_CASE("Matrix3x3 inequality", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    Matrix3x3 m2 = Matrix3x3(-14, 96, 45, -140, 66, 96, -51, 36, -33);
+    CHECK(m1 != m2);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m2 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    CHECK_FALSE(m1 != m2);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m2 = Matrix3x3(-27, 83, 33, -153, 53, 83, -64, 23, -46);
+    CHECK(m1 != m2);
+}
+
+TEST_CASE("Matrix3x3 transpose", "[Matrix3x3]")
+{
+    // Case 1
+    Matrix3x3 m1 = Matrix3x3(2, -5, 3, 7, 1, -6, -9, 4, 8);
+    m1 = Matrix3x3::Transpose(m1);
+    Matrix3x3 m2 = Matrix3x3(2, 7, -9, -5, 1, 4, 3, -6, 8);
+    CHECK_MATRIX(m1, m2);
+    // Case 2
+    m1 = Matrix3x3(0.24, 0.0082, -0.3, 0.6, -1.4, 0.73, 0.38, 0.096, -0.16);
+    m1 = Matrix3x3::Transpose(m1);
+    m2 = Matrix3x3(0.24, 0.6, 0.38, 0.0082, -1.4, 0.096, -0.3, 0.73, -0.16);
+    CHECK_MATRIX(m1, m2);
+    // Case 3
+    m1 = Matrix3x3(-27, 83, 32, -153, 53, 83, -64, 23, -46);
+    m1 = Matrix3x3::Transpose(m1);
+    m2 = Matrix3x3(-27, -153, -64, 83, 53, 23, 32, 83, -46);
+    CHECK_MATRIX(m1, m2);
+}
